1592d1fb9e71b9a6cf389b5df4c1173e301a96dc
p-pranitha <pranithapalaparthi2007@gmail.com>
2026-08-14 12:43:06 +0530

    Initial commit

---
+++ week4/gencon.js
@@ -0,0 +1,6 @@
+"use strict";
+function checkLength(value) {
+    console.log("Length:", value.length);
+}
+checkLength("PRANITHA");
+checkLength([10, 20, 30]);
---
+++ week4/gencon.ts
@@ -0,0 +1,13 @@
+interface LengthCheck{
+    length:number;
+}
+
+
+function checkLength<T extends LengthCheck>(value:T){
+
+    console.log("Length:",value.length);
+
+}
+
+checkLength("PRANITHA");
+checkLength([10,20,30]);
